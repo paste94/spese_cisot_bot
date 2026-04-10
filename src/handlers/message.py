37e6ccf1b4ff89b97e67dc13b07064ff75b9bf6f
RPASTERIS <r.pasteris-itf@trenitalia.it>
2026-04-10 15:53:16 +0200

    State mempry storage converted to async

--- src/handlers/message.py
+++ src/handlers/message.py
@@ -7,6 +7,8 @@ from services.gsheet.utils import add_row
 from services.utilities import handle_errors, parse_message
 from services.users.users import USERS
 
+active_timers: dict = {}
+
 @bot.message_handler(func=lambda msg: True)
 @handle_errors(bot)
 def get_message(message):
@@ -22,44 +24,52 @@ def get_message(message):
     if row['split'] == False:
         markup = InlineKeyboardMarkup(row_width=2)
         markup.add(
-            InlineKeyboardButton("✅ Sì", callback_data="si"),
-            InlineKeyboardButton("❌ No (Default)", callback_data="no")
+            InlineKeyboardButton("✅ Sì", callback_data="si_split"),
+            InlineKeyboardButton("❌ No (Default)", callback_data="no_split")
         )
         
         bot.send_message(chat_id, f"🤔 Vuoi che questa spesa sia divisa? ({TIMEOUT_SECONDS}s timeout)", reply_markup=markup)
         bot.set_state(user_id , MessageState.waiting_split_decision, chat_id)
 
-        timer = threading.Timer(
-            TIMEOUT_SECONDS,
-            on_timeout,
-            args=[chat_id, user_id, username]
-        )
-        timer.start()
-
         with bot.retrieve_data(user_id, chat_id) as data:
-            data["timer"] = timer
+            # data["timer"] = timer
             data["row"] = row
+
+        start_timeout(chat_id, user_id, username, TIMEOUT_SECONDS)
     else:
         handle_add_row(row, username, chat_id, user_id)
 
-@bot.message_handler(state=MessageState.waiting_split_decision)
+@bot.callback_query_handler(func=lambda call: call.data in ["si_split", "no_split"])
 @handle_errors(bot)
-def get_message(message):
-    if not USERS.is_authorized(message.from_user.username):
-        bot.reply_to(message, "❌ Non sei autorizzato a inviare messaggi.")
-        return
+def handle_split_response(call):
+    user_id = call.from_user.id
+    with bot.retrieve_data(user_id, call.message.chat.id) as data:
+        row = data.get("row")
+
+    if call.data == "si_split":
+        row["split"] = True
     
-    chat_id = message.chat.id
-    user_id = message.from_user.id
-    username = message.from_user.username
+    handle_add_row(row, call.from_user.username, call.message.chat.id, user_id)
+    bot.delete_state(user_id, call.message.chat.id)
 
-    with bot.retrieve_data(user_id, chat_id) as data:
-        row = data["row"]
-        data['row'] = None
+# @bot.message_handler(state=MessageState.waiting_split_decision)
+# @handle_errors(bot)
+# def get_message(message):
+#     if not USERS.is_authorized(message.from_user.username):
+#         bot.reply_to(message, "❌ Non sei autorizzato a inviare messaggi.")
+#         return
+    
+#     chat_id = message.chat.id
+#     user_id = message.from_user.id
+#     username = message.from_user.username
 
-    if message.data == "si":
-        row['split'] = True 
-    handle_add_row(row, username, chat_id, user_id)
+#     with bot.retrieve_data(user_id, chat_id) as data:
+#         row = data["row"]
+#         data['row'] = None
+
+#     if message.data == "si":
+#         row['split'] = True 
+#     handle_add_row(row, username, chat_id, user_id)
 
 def on_timeout(chat_id: str, user_id: str, username: str):
     with bot.retrieve_data(user_id, chat_id) as data:
@@ -67,6 +77,20 @@ def on_timeout(chat_id: str, user_id: str, username: str):
     handle_add_row(row, username, chat_id, user_id)
 
 def handle_add_row(row, username, chat_id, user_id):
+    cancel_timeout(user_id)
     add_row(row, username)
     bot.delete_state(user_id, chat_id)
     bot.send_message(chat_id, f"✅ Spesa aggiunta: {row['description']} - {row['price']}€ {'(diviso)' if row['split'] else ''}")
+
+def start_timeout(chat_id, user_id, username, seconds):
+    # Cancella eventuale timer precedente
+    cancel_timeout(user_id)
+
+    timer = threading.Timer(seconds, on_timeout, args=[chat_id, user_id, username])
+    timer.start()
+    active_timers[user_id] = timer
+
+def cancel_timeout(user_id):
+    if user_id in active_timers:
+        active_timers[user_id].cancel()
+        del active_timers[user_id]
