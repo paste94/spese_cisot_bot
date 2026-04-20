892f43a319ce8dd39e370c527ff643bd9233e1b7
RPASTERIS <r.pasteris-itf@trenitalia.it>
2026-04-20 09:54:53 +0200

    Back to previews version

--- src/scripts/run.sh
+++ src/scripts/run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 source /home/pi/Applications/spese_cisot_bot/venv/bin/activate
 python -u /home/pi/Applications/spese_cisot_bot/spese_cisot_bot.py
+# python -u /home/pi/Applications/spese_cisot_bot/spese_cisot_bot.py
 
 # Questo sistema è schedulato con systemctl, guardare il README in service per info. 
 # Dopo una modifica al codice eseguire "systemctl --user restart telegram-bot.service" per renderlo effettivo
