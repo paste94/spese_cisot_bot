1d040d5b658ca00e8bffc7f287d7d4af9ae8dec5
RPASTERIS <r.pasteris-itf@trenitalia.it>
2026-04-21 15:49:07 +0200

    Added state cleaner

--- src/bot_instance.py
+++ src/bot_instance.py
@@ -5,6 +5,10 @@ from dotenv import load_dotenv
 from telebot import TeleBot, types
 from telebot.storage import StateMemoryStorage
 import telebot.apihelper as apihelper
+import threading
+import time
+from services.logger.logger import logger
+
 
 load_dotenv()
 TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
@@ -12,6 +16,20 @@ TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 apihelper.SESSION_TIME_TO_LIVE = 5 * 60  # ricrea la sessione ogni 5 minuti
 
 state_storage = StateMemoryStorage()
+
+def clean_storage():
+    try:
+        while True:
+            time.sleep(3600)  # ogni ora
+            state_storage.data.clear()  # svuota tutti gli stati
+            logger.info("State storage pulito")
+    except Exception as e:
+        logger.error("Errore durante la pulizia dello storage: %s", e)
+
+
+cleaner = threading.Thread(target=clean_storage, daemon=True)
+cleaner.start()
+
 bot = TeleBot(TOKEN, state_storage=state_storage)
 bot.set_my_commands([
     types.BotCommand("help", "🆘 Guida rapida"),
